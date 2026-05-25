850d919d30f330df004b17ef5426f3ccea6d4819
Rônero G. Cavalcante <ronero.cavalcante@itec.al.gov.br>
2026-05-25 10:02:27 -0300

    chore: primeira configuração do projeto

---
+++ src/main/java/br/com/ronero/plataformaservicos/PlataformaServicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ronero.plataformaservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaServicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaServicosApplication.class, args);
+    }
+
+}
